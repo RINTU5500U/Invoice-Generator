287f7694b7a7f86a680efdf039ea7a427fe7de28
RINTU5500U <crowdrintu1@gmail.com>
2024-02-21 16:32:49 +0530

    done

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+require('dotenv').config();
 const express = require("express")
 const mongoose = require("mongoose")
-require('dotenv').config();
 const route = require("./src/routes/route")
 const app = express()
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 
-mongoose.connect(`mongodb+srv://BiswajitSwain:${process.env.MONGODB_PASSWORD}@cluster0.xf1eq.mongodb.net/invoice_generator`,
-    { usenewUrlParser: true })
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB is connected"))
     .catch((err) => console.log(err.message))
 
 app.use("/", route)
 
-app.listen(3000, () => {
-    console.log("Express is running on port " + 3000)
+app.listen(PORT, () => {
+    console.log("Express is running on port " + PORT)
 })
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,14 +5,17 @@ const {register, login, addProduct, viewQuotations} = require('../controllers/us
 const {authentication, authorization} = require('../middlewares/auth')
 const { userValidation, loginValidation, productValidation } = require("../middlewares/validationMware")
 
+router.get('/hello', (req, res) => {
+    return res.send('welcome to the invoice generator')
+})
 router.post('/register', userValidation, register)
 router.post('/login', loginValidation, login)
 router.post('/addProduct/:userId', authentication, productValidation, addProduct)
 router.get('/viewQuotations/:userId', authentication, authorization, viewQuotations)
 // router.get('/getInvoicePdf/:userId', authentication, authorization, getInvoicePdf)
 
-router.all("/*", function (req, res) { 
-    return res.status(400).send({ status: false, message: "invalid http request" });
-});
+// router.all("/*", function (req, res) { 
+//     return res.status(400).send({ status: false, message: "invalid http request" });
+// });
 
 module.exports = router
